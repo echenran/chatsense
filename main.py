a005686a678a897aa589bddac4fa6c03be043182
ECR <emchenran@gmail.com>
2017-12-02 15:22:27 -0500

    refactor; m4a->wav => 'Not a WAV file' error (converted file is 0B), mp3->wav => fine, wav => fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, flash, redirect, session, abort, render_template, request
+from flask import Flask, flash, redirect, request
 import json
+from processaudioapp import processaudio
 app = Flask(__name__)
 
 @app.route('/send', methods=['GET', 'POST'])
@@ -7,11 +8,19 @@ def process_send():
     print "[Request headers]:", request.headers
     print "[Request data]:", len(request.data)
     print "[Request form]:", request.form
-    return "Received\n"
+    
+    if request.data:
+        res = processaudio.analyze(request.data)
+
+    return "Received\nProcessed:",res
 
 @app.route('/getback', methods=['GET', 'POST'])
 def process_getback():
     print "[Request headers]:", request.headers
     print "[Request data]:", len(request.data)
     print "[Request form]:", request.form
+    
+    if request.data:
+        print "[Request data keys]:", request.data.keys()
+
     return "Not done yet\n"
--- processaudio.py
+++ processaudio.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # measure_wav_mac.py
 # Paul Boersma 2017-09-17
 #
@@ -13,70 +15,108 @@
 #   Sad: 0.238
 #   Angry: 0.001
 #   Fear: 0.000
-
+from flask import Blueprint
 import sys, os
 import scipy.io.wavfile
+from pydub import AudioSegment
 
 # Constants
-vokaturi = "./vokaturi"
-audiotranscodelib = "./python-audiotranscode"
 WAV = "wav"
+VOKATURI = "./vokaturi"
+AUDIOTRANSCODELIB = "./python-audiotranscode"
 
-# Load Vokaturi
-sys.path.append(vokaturi + "/api")
-import Vokaturi
-print ("Loading Vokaturi...")
-Vokaturi.load(vokaturi + "/lib/Vokaturi_mac.so")
+class ProcessAudio(object):
 
-# Load audiotranscode
-sys.path.append(audiotranscodelib)
-import audiotranscode
-print ("Loading audiotranscode...")
-at = audiotranscode.AudioTranscode()
+    # Load libraries
+    sys.path.append(VOKATURI + "/api")
+    import Vokaturi as vokaturilib
+    vok = vokaturilib
+    sys.path.append(AUDIOTRANSCODELIB)
+    import audiotranscode as audiotranscodelib
+    audtrans = audiotranscodelib
 
-# Load + convert audio
-oldfilename = newfilename = sys.argv[1]
-print ("Loading sound file <{}>...".format(oldfilename))
-if oldfilename.split(".")[1].lower() != WAV:
-    print ("Converting sound file from .{} to .{}...".format(oldfilename.split(".")[1], WAV))
-    newfilename = oldfilename.split(".")[0] + "." + WAV
-    at.transcode(oldfilename, newfilename)
-    print("oldfilename: {} newfilename: {} ({} bytes)".format(oldfilename, newfilename, os.path.getsize("./"+newfilename)))
+    def __init__(self):
+        # Load Vokaturi
+        print ("Loading Vokaturi...")
+        self.vok.load(VOKATURI + "/lib/Vokaturi_mac.so")
 
-# Read audio
-print ("Reading sound file...")
-(sample_rate, samples) = scipy.io.wavfile.read(newfilename)
-print ("   sample rate %.3f Hz" % sample_rate)
+        # Load audiotranscode
+        print ("Loading audiotranscode...")
+        self.at = self.audtrans.AudioTranscode()
 
-print ("Allocating Vokaturi sample array...")
-buffer_length = len(samples)
-print ("   %d samples, %d channels" % (buffer_length, samples.ndim))
-c_buffer = Vokaturi.SampleArrayC(buffer_length)
-if samples.ndim == 1:  # mono
-	c_buffer[:] = samples[:] / 32768.0
-else:  # stereo
-	c_buffer[:] = 0.5*(samples[:,0]+0.0+samples[:,1]) / 32768.0
+        self.filename = None
 
-print ("Creating VokaturiVoice...")
-print("size of {}: {} bytes".format(newfilename, os.path.getsize(newfilename)))
-voice = Vokaturi.Voice (sample_rate, buffer_length)
+    def load(self, filepath):
+        # Load + convert audio
+        """oldfilename = newfilename = filepath.split("/")[-1]
+        print ("Loading sound file <{}>...".format(oldfilename))
+        if oldfilename.split(".")[-1].lower() != WAV:
+            print ("Converting sound file from .{} to .{}...".format(oldfilename.split(".")[-1], WAV))
+            newfilename = oldfilename.split(".")[0] + "." + WAV
+            self.at.transcode(filepath, newfilename)
+            self.filename = newfilename
 
-print ("Filling VokaturiVoice with samples...")
-print("size of {}: {} bytes".format(newfilename, os.path.getsize(newfilename)))
-voice.fill(buffer_length, c_buffer)
+            print("oldfilename: {} newfilename: {} ({} bytes)".format(oldfilename, newfilename, os.path.getsize("./"+newfilename)))
+        else:
+            self.filename = filepath"""
+        oldfilename = newfilename = filepath.split("/")[-1]
+        print ("Loading sound file <{}>...".format(oldfilename))
+        if oldfilename.split(".")[-1].lower() != WAV:
+            print ("Converting sound file from .{} to .{}...".format(oldfilename.split(".")[-1], WAV))
+            newfilename = oldfilename.split(".")[0] + "." + WAV
+            old = AudioSegment.from_mp3(filepath)
+            old.export(newfilename, format=WAV)
+            self.filename = newfilename
 
-print ("Extracting emotions from VokaturiVoice...")
-quality = Vokaturi.Quality()
-emotionProbabilities = Vokaturi.EmotionProbabilities()
-voice.extract(quality, emotionProbabilities)
+            print("oldfilename: {} newfilename: {} ({} bytes)".format(oldfilename, newfilename, os.path.getsize("./"+newfilename)))
+        else:
+            self.filename = filepath
 
-if quality.valid:
-	print ("Neutral: %.3f" % emotionProbabilities.neutrality)
-	print ("Happy: %.3f" % emotionProbabilities.happiness)
-	print ("Sad: %.3f" % emotionProbabilities.sadness)
-	print ("Angry: %.3f" % emotionProbabilities.anger)
-	print ("Fear: %.3f" % emotionProbabilities.fear)
-else:
-	print ("Not enough sonorancy to determine emotions")
 
-voice.destroy()
+    def analyze(self):
+        # Read audio
+        print ("Reading sound file...")
+        (sample_rate, samples) = scipy.io.wavfile.read(self.filename)
+        print ("   sample rate %.3f Hz" % sample_rate)
+
+        print ("Allocating Vokaturi sample array...")
+        buffer_length = len(samples)
+        print ("   %d samples, %d channels" % (buffer_length, samples.ndim))
+        c_buffer = self.vok.SampleArrayC(buffer_length)
+        if samples.ndim == 1:  # mono
+            c_buffer[:] = samples[:] / 32768.0
+        else:  # stereo
+            c_buffer[:] = 0.5*(samples[:,0]+0.0+samples[:,1]) / 32768.0
+
+        print ("Creating VokaturiVoice...")
+        print("size of {}: {} bytes".format(self.filename, os.path.getsize(self.filename)))
+        voice = self.vok.Voice (sample_rate, buffer_length)
+
+        print ("Filling VokaturiVoice with samples...")
+        print("size of {}: {} bytes".format(self.filename, os.path.getsize(self.filename)))
+        voice.fill(buffer_length, c_buffer)
+
+        print ("Extracting emotions from VokaturiVoice...")
+        quality = self.vok.Quality()
+        emotionProbabilities = self.vok.EmotionProbabilities()
+        voice.extract(quality, emotionProbabilities)
+
+        res = {}
+        if quality.valid:
+            res['neutrality'] = emotionProbabilities.neutrality
+            res['happiness']  = emotionProbabilities.happiness
+            res['sadness']    = emotionProbabilities.sadness
+            res['anger']      = emotionProbabilities.anger
+            res['fear']       = emotionProbabilities.fear
+        else:
+            print ("Not enough sonorancy to determine emotions")
+
+        voice.destroy()
+
+        return res
+
+obj = ProcessAudio()
+#f = "./vokaturi/examples/hey-sweetness.wav"
+f = "../../Downloads/hurtme.mp3"
+obj.load(f)
+print(obj.analyze())
