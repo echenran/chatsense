f2c4bf258f070bd69c16dc1b61a685232ad5a580
Jake <jbildy@uwo.ca>
2017-12-02 23:46:27 -0500

    Updated audio

--- app/src/main/java/com/chatsense/chatsense/Chat.java
+++ app/src/main/java/com/chatsense/chatsense/Chat.java
@@ -1,6 +1,7 @@
 package com.chatsense.chatsense;
 
 import android.media.AudioFormat;
+import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
@@ -22,7 +23,7 @@ import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Chat extends AppCompatActivity implements View.OnClickListener{
+public class Chat extends AppCompatActivity implements View.OnFocusChangeListener, View.OnClickListener{
 
     boolean doRun = true;
     boolean isRecording = false;
@@ -52,14 +53,16 @@ public class Chat extends AppCompatActivity implements View.OnClickListener{
 
         //Recording button
         final Button recordButton = (Button) findViewById(R.id.record);
+        recordButton.setOnFocusChangeListener(this);
         recordButton.setOnClickListener(this);
+
     }
 
 
     public void startRecording(View view) throws IOException
     {
         /**Records audio, outputs .mp3 file**/
-         recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLER_RATE, AudioFormat.CHANNEL_IN_MONO,
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLER_RATE, AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT, BUFFER_ELEM_TO_REC*BYTES_PER_ELEM);
 
         recorder.startRecording();
@@ -72,7 +75,7 @@ public class Chat extends AppCompatActivity implements View.OnClickListener{
             }
         }, "AudioRec thread");
 
-         recordingThread.start();
+        recordingThread.start();
 
     }
 
@@ -121,6 +124,21 @@ public class Chat extends AppCompatActivity implements View.OnClickListener{
         }
     }
 
+    public void playRecording(View view)
+    {
+        try {
+            MediaPlayer player = new MediaPlayer();
+            player.setDataSource(filePath);
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            player.prepare();
+            player.start();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     public void stopRecording(View view)
     {
         if (null != recorder) {
@@ -134,52 +152,29 @@ public class Chat extends AppCompatActivity implements View.OnClickListener{
 
     boolean notRunning = true;
 
-    public void onClick(final View view)
+    public void onClick(View view)
     {
         if (view.getId() == R.id.record) {
-            final ProgressBar progress = (ProgressBar) findViewById(R.id.progressBar);
-
-            final Timer timer = new Timer();
-
             try {
                 startRecording(view);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            if (notRunning) {
-                notRunning = false;
-                timer.scheduleAtFixedRate(new TimerTask() {
-                    int value = 0;
-
-                    @Override
-                    public void run() {
-                        progress.setProgress(++value);
-
-                        if (value > 100)
-                            timer.cancel();
-                    }
-                }, 100, 100);
-                notRunning = true;
-                stopRecording(view);
-
-            }
-        }
-        if (view.getId() == R.id.playButton)
-        {
-            try {
-                MediaPlayer player = new MediaPlayer();
-                player.setDataSource(filePath);
-                player.prepare();
-                player.start();
             }
             catch (Exception e)
             {
                 e.printStackTrace();
             }
+        }
+        if (view.getId() == R.id.playButton)
+        {
+            playRecording(view);
 
         }
     }
 
+    public void onFocusChange(View view, Boolean hasFocus)
+    {
+        if (hasFocus == false)
+        stopRecording(view);
+
+    }
 
 }
--- app/src/main/java/com/chatsense/chatsense/EmojiChat.java
+++ app/src/main/java/com/chatsense/chatsense/EmojiChat.java
@@ -1,24 +1,26 @@
 package com.chatsense.chatsense;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.rockerhieu.emojicon.EmojiconTextView;
+
+import java.util.zip.Inflater;
 
 /**
  * Created by Jacob on 2017-12-02.
  */
 
-public class EmojiChat extends LinearLayout{
 
-
-    public EmojiChat(Context context) {
-        super(context);
-
-        loadViews();
-    }
-
-    private void loadViews()
+public class EmojiChat
+{
+    public void initializeEmojiChat()
     {
 
     }
