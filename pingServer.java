5e01a209fddc3b6396a5f3017d6339dc40e6bf90
ECR <emchenran@gmail.com>
2017-12-02 22:20:50 -0500

    add test line

--- pingServer.java
+++ pingServer.java
@@ -48,6 +48,7 @@ public class pingServer
 			// Use a post method
 			conn.setRequestMethod("POST");
 			conn.setRequestProperty("Content-Type", "Sending file");
+			conn.addRequestProperty("num_messages", "20");
 
 			dos = new DataOutputStream(conn.getOutputStream());
 			
