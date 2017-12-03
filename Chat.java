5d2e33f2eea54e16abab126c254f4bedf14c4ad9
Charles Rule <cruleis@gmail.com>
2017-12-03 10:24:27 -0500

    Add files via upload

--- Chat.java
+++ Chat.java
@@ -1,37 +1,42 @@
 package com.chatsense.chatsense;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.rockerhieu.emojicon.EmojiconTextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
-public class Chat extends AppCompatActivity implements View.OnFocusChangeListener, View.OnClickListener{
+
+public class Chat extends AppCompatActivity implements  View.OnClickListener{
 
     boolean doRun = true;
     boolean isRecording = false;
@@ -45,13 +50,17 @@ public class Chat extends AppCompatActivity implements View.OnFocusChangeListene
     Thread recordingThread = null;
     public String filePath;
 
+    String returnData;
+    String chatLogStr;
+
+    //Loads the name of the user you're texting
+    TextView userTexting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        //Loads the name of the user you're texting
-        TextView userTexting = (TextView) findViewById(R.id.userTexting);
+        userTexting = (TextView) findViewById(R.id.userTexting);
         Bundle extras = getIntent().getExtras();
         userTexting.setText(extras.getString("whose_mans"));
 
@@ -61,85 +70,13 @@ public class Chat extends AppCompatActivity implements View.OnFocusChangeListene
 
         //Recording button
         final Button recordButton = (Button) findViewById(R.id.record);
-        recordButton.setOnFocusChangeListener(this);
         recordButton.setOnClickListener(this);
 
-    }
-
-    private static String lineEnd = "\r\n";
-    private static String twoHyphens = "--";
-    private static String boundary = "AaB03x87yxdkjnxvi7";
-    private static URL url;
-    private static String emotion_figures;
-
-    public void upload() throws IOException
-    {
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null;
-        DataInputStream dis = null;
-        FileInputStream fileInputStream = null;
-        String fileParameterName = filePath;
-        url = new URL("http://chatsense.pythonanywhere.com/getback");
-        byte[] buffer;
-        int maxBufferSize = 20 * 1024;
-        try {
-            //client request
-            File file = new File(fileParameterName);
-            fileInputStream = new FileInputStream(file);
-
-            // open a URL connection to the Servlet
-            // Open a HTTP connection to the URL
-            conn = (HttpURLConnection) url.openConnection();
-            // Allow inputs
-            conn.setDoInput(true);
-            // Allow outputs
-            conn.setDoOutput(true);
-            // Don't use a cached copy
-            conn.setUseCaches(false);
-            // Use a post method
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "Sending file");
-            conn.addRequestProperty("num_messages", "20");
-
-            dos = new DataOutputStream(conn.getOutputStream());
-
-            dos.writeBytes("Content-Disposition: name=\"" + file.toString()
-                    + lineEnd);
-
-			// create a buffer of maximum size
-			buffer = new byte[Math.min((int) file.length(), maxBufferSize)];
-			int length;
-			// read file and write it into form
-			while ((length = fileInputStream.read(buffer)) != -1) {
-				dos.write(buffer, 0, length);
-			}
-
-            //send form data necessary after file data
-            dos.writeBytes(lineEnd);
-
-            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-            dos.flush();
-        } finally {
-            if (fileInputStream != null) fileInputStream.close();
-            if (dos != null) dos.close();
-        }
-
-        //reads the server response
-        try {
-            dis = new DataInputStream(conn.getInputStream());
-            StringBuilder response = new StringBuilder();
-
-            String line;
-            while ((line = dis.readLine()) != null) {
-                response.append(line).append('\n');
-            }
-
-            emotion_figures = response.toString();
-        } finally {
-            if (dis != null) dis.close();
-        }
+        final ImageButton playButton = (ImageButton) findViewById(R.id.playButton);
+        playButton.setOnClickListener(this);
 
     }
+
 
     public void startRecording(View view) throws IOException
     {
@@ -175,8 +112,7 @@ public class Chat extends AppCompatActivity implements View.OnFocusChangeListene
     }
 
     private void writeAudioDataToFile() {
-        filePath = Environment.getExternalStorageDirectory().getPath()+ File.separator +
-                "/chatsense/message.pcm";
+        filePath = Environment.getExternalStorageDirectory().getPath()+ File.separator + "message.pcm";
         short sData[] = new short[BUFFER_ELEM_TO_REC];
         /** Java can't use shorts, need to convert to bytes**/
 
@@ -238,7 +174,9 @@ public class Chat extends AppCompatActivity implements View.OnFocusChangeListene
     {
         if (view.getId() == R.id.record) {
             try {
-                startRecording(view);
+                if (!isRecording) {
+                    startRecording(view);
+                }
             }
             catch (Exception e)
             {
@@ -247,15 +185,144 @@ public class Chat extends AppCompatActivity implements View.OnFocusChangeListene
         }
         if (view.getId() == R.id.playButton)
         {
-            playRecording(view);
+            if (isRecording) {
+                stopRecording(view);
 
+                SendFeedbackJob job = new SendFeedbackJob();
+                job.execute();
+
+                try {
+                    while (returnData == null)
+                    {
+                        Thread.sleep(1000);
+                    }
+                    try {
+                        initializeEmojiChat(returnData);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
         }
     }
 
-    @Override
-    public void onFocusChange(View view, boolean b) {
-        if (b == false)
-            stopRecording(view);
+    public void onFocusChange(View view, Boolean hasFocus)
+    {
+        if (!hasFocus)
+        stopRecording(view);
+
+    }
+    private class SendFeedbackJob extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String s = "";
+            String chatlog = "";
+            try {
+                s = PingServer.upload();
+            }
+            catch (Exception e) {
+            e.printStackTrace();
+            }
+
+            chatLogStr = chatlog;
+            returnData = s;
+            return s;
+        }
     }
 
+    public static Drawable setTint(Drawable drawable, int color) {
+        final Drawable newDrawable = DrawableCompat.wrap(drawable);
+        DrawableCompat.setTint(newDrawable, color);
+        return newDrawable;
+    }
+
+    public void initializeEmojiChat(String data) throws InterruptedException, JSONException {
+        returnData = null;
+        View text = LayoutInflater.from(this).inflate(R.layout.emojichat, null);
+        LinearLayout chat_history = (LinearLayout) findViewById(R.id.TEXT_HISTORY) ;
+        chat_history.addView(text);
+
+        JSONObject obj = new JSONObject(data);
+        String textVar = obj.getString("text");
+        String time = obj.getString("timestr").substring(1);
+        JSONObject emotions = new JSONObject(obj.getString("emotions"));
+        JSONObject color = new JSONObject(obj.getString("color"));
+
+        int neutrality = emotions.getInt("neutrality");
+        int anger = emotions.getInt("anger");
+        int happiness = emotions.getInt("happiness");
+        int sadness = emotions.getInt("sadness");
+        int fear = emotions.getInt("fear");
+
+        int red = color.getInt("red");
+        int blue = color.getInt("blue");
+        int alpha = color.getInt("alpha");
+        int green = color.getInt("green");
+
+        EmojiconTextView emoji = (EmojiconTextView) text.findViewById(R.id.emoji);
+        Button bubble = (Button) text.findViewById(R.id.chat_bubble);
+        TextView date = (TextView) text.findViewById(R.id.date);
+
+        bubble.setText(textVar);
+        date.setText(time);
+        emoji.setText(getEmojiFromValues(neutrality,happiness,sadness,anger,fear));
+        bubble.setBackgroundColor(Color.argb(alpha,red,green,blue));
+
+    }
+
+    public String getEmojiFromValues(int neutral, int happy, int sad, int angry, int fear) {
+        String NEUTRAL = "\uD83D\uDE36";
+
+        String[] SAD = {"\uD83D\uDE14","\uD83D\uDE1E","\uD83D\uDE2D"};
+
+        String[] FEAR = {"\uD83D\uDE2C","\uD83D\uDE28","\uD83E\uDD14"};
+
+        String[] HAPPY = {"\uD83D\uDE0A","\uD83D\uDE03","\uD83D\uDE02"};
+
+        String[] ANGRY = {"\uD83D\uDE12","\uD83D\uDE20","\uD83D\uDE21"};
+
+        int i;
+        if (neutral > 85)
+        {
+            return NEUTRAL;
+        }
+        if (neutral > 55)
+        {
+            i = 0;
+        }
+        if (neutral > 35)
+        {
+            i = 1;
+        }
+        else
+        {
+            i = 2;
+        }
+
+
+        int max = Integer.max(Integer.max(sad, happy),Integer.max(angry, fear));
+        if (happy == max)
+        {
+            return HAPPY[i];
+        }
+        if (sad == max)
+        {
+            return SAD[i];
+        }
+        if (angry == max)
+        {
+            return ANGRY[i];
+        }
+        if (fear == max)
+        {
+            return FEAR[i];
+        }
+
+
+        return NEUTRAL;
+    }
 }
